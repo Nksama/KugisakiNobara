c561415ecc58a66e9243134f24ca5f8aab0d7f5e
UNKNOWN <84973544+6456yrgfbfhg@users.noreply.github.com>
2021-07-18 07:18:24 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -271,7 +271,7 @@ def info(update: Update, context: CallbackContext):
     disaster_level_present = False
 
     if user.id == OWNER_ID:
-        text += "\n\nThe Disaster level of this person is 'God'."
+        text += "\n\nThe Disaster level of this person is 'GOD'."
         disaster_level_present = True
     elif user.id in DEV_USERS:
         text += "\n\nThis user is member of 'Hero Association'."
@@ -290,7 +290,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchUpdates/155">?</a>]'.format(
+        text += ' [<a href="https://t.me/nobara_kugisaki_updates/8">?</a>]'.format(
             bot.username)
 
     try:
@@ -316,6 +316,7 @@ def info(update: Update, context: CallbackContext):
 
     if INFOPIC:
           if INFOPIC:
+        if INFOPIC:
         try:
             profile = context.bot.get_user_profile_photos(user.id).photos[0][-1]
             context.bot.sendChatAction(chat.id, "upload_photo")
