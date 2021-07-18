5820a3f1f78996bdf84fb70b17aa8ee3aa05a4cb
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-07-18 08:32:54 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -94,7 +94,7 @@ if ENV:
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
-    
+    BOT_ID = os.environ.get('BOT_ID' , None)
     
     try:
         BL_CHATS = set(int(x) for x in os.environ.get('BL_CHATS', "").split())
