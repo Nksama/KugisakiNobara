ba9508fb647c6facfd213a4848b6f03d0a0bc02f
UNKNOWN <84973544+6456yrgfbfhg@users.noreply.github.com>
2021-07-22 10:23:16 +0530

    Update fumostrings.py

--- SaitamaRobot/modules/fumostrings.py
+++ SaitamaRobot/modules/fumostrings.py
@@ -22,7 +22,10 @@ FUMO_STICKERS = [
      "CAACAgEAAxkBAAEHlWhg-PRFu7j-2HQngZG2II_7aCZx2gACKgADUypEPAeT3YifKneTIAQ",
      "CAACAgEAAxkBAAEHlY1g-Pb8JBtp8fJTEUvXEKrG5XHxDgACXAADUypEPBRSMOhTJ24BIAQ"
    ]
- FUMO_TEXTS = [
+ 
+      
+      
+      FUMO_TEXTS = [
        "{user1} Gifts {user2} A Fummo Doll",
        "{user1} Headpats {user2}",
        "{user2} Got Fumofied",
