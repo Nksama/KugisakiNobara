2993b3dabe4499359e429a96511768e1ac8b6957
UNKNOWN <84973544+6456yrgfbfhg@users.noreply.github.com>
2021-07-22 10:45:29 +0530

    Update fumostrings.py

--- SaitamaRobot/modules/fumostrings.py
+++ SaitamaRobot/modules/fumostrings.py
@@ -24,12 +24,11 @@ FUMO_STICKERS = [
    ]
  
       
-      
-      FUMO_TEXTS = [
-       "{user1} Gifts {user2} A Fummo Doll",
-       "{user1} Headpats {user2}",
-       "{user2} Got Fumofied",
-       "{user1} Reincarnated As a Fumo",
-       "{user2} Mofu Mofu!"
-    ]
+FUMO_TEXTS = 
+     "{user1} Gifts {user2} A Fummo Doll",
+     "{user1} Headpats {user2}",
+     "{user2} Got Fumofied",
+     "{user1} Reincarnated As a Fumo",
+     "{user2} Mofu Mofu!"
+  ]
       
