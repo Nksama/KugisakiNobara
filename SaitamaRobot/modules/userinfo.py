e6941f0454f6eef2375f648c7eddea37891dee56
UNKNOWN <84973544+6456yrgfbfhg@users.noreply.github.com>
2021-08-03 18:33:25 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -290,7 +290,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchUpdates/155">?</a>]'.format(
+        text += ' [<a href="https://t.me/nobara_kugisaki_updates/5">?</a>]'.format(
             bot.username)
 
     try:
