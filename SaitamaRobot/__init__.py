3230ac4520c6cd9f36a5618bd5285c650356022c
UNKNOWN <84973544+6456yrgfbfhg@users.noreply.github.com>
2021-08-05 09:04:57 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -81,8 +81,8 @@ if ENV:
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
     STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
     WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER',
-                                 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
+    BAN_STICKER = os.environ.get('BAN_STICKER')
+    ARQ_API = os.environ.get('ARQ_API')           
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     CASH_API_KEY = os.environ.get('CASH_API_KEY', None)
     TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
