5533155b01af666e56b38966964940c2932e524c
UNKNOWN <84973544+6456yrgfbfhg@users.noreply.github.com>
2021-08-07 11:56:26 +0530

    Update stickers.py

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -520,7 +520,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./Cutiepii_Robot/resources/ArmWrestler.ttf"
+        fnt = "./SaitamaRobot/resources/ArmWrestler.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
